fecd9d7e4c7642d50b4dce65474033e412b25872
Noah Bruno <noah.bruno122@gmail.com>
2022-03-15 09:58:10 -0500

    Blackjack

--- src/BlackJack/Deck.java
+++ src/BlackJack/Deck.java
@@ -1,5 +1,29 @@
 package BlackJack;
 
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        this.cards = new ArrayList<Card>();
+    }
+
+    public void createNewDeck(){
+        for(Suit cardSuit : Suit.values()){
+            for(Value cardValue : Value.values()){
+                this.cards.add(new Card(cardSuit, cardValue));
+            }
+        }
+    }
+
+    public String toString(){
+        String cardListOutput = "";
+        int i = 0;
+        for (Card aCard : this.cards){
+            cardListOutput += "\n" + i + "-" + aCard.toString();
+            i++;
+        }
+        return cardListOutput;
+    }
 }
--- src/BlackJack/Main.java
+++ src/BlackJack/Main.java
@@ -3,5 +3,9 @@ package BlackJack;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("welcome to BlackJack!");
+
+        Deck playingDeck = new Deck();
+        playingDeck.createNewDeck();
     }
 }
